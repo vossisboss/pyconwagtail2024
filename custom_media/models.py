af630d31686f0ea6f4813a7b2e55e4381e5e79a0
Scott Cranfill <scott@scottcranfill.com>
2024-05-13 22:29:05 -0400

    Update custom image model from template

--- custom_media/models.py
+++ custom_media/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from wagtail.documents.models import Document, AbstractDocument
 
 from wagtail.images.models import Image, AbstractImage, AbstractRendition
 
@@ -9,18 +8,18 @@ class CustomImage(AbstractImage):
 
     # To add a caption field:
     # caption = models.CharField(max_length=255, blank=True)
-    default_alt_text = models.CharField(max_length=255, blank=False)
+    default_alt_text = models.CharField(max_length=255, blank=True)
 
     admin_form_fields = Image.admin_form_fields + (
         # Then add the field names here to make them appear in the form:
-        # 'caption',
+        "default_alt_text",
     )
 
 
 class CustomRendition(AbstractRendition):
-    image = models.ForeignKey(CustomImage, on_delete=models.CASCADE, related_name='renditions')
+    image = models.ForeignKey(
+        CustomImage, on_delete=models.CASCADE, related_name="renditions"
+    )
 
     class Meta:
-        unique_together = (
-            ('image', 'filter_spec', 'focal_point_key'),
-        )
+        unique_together = (("image", "filter_spec", "focal_point_key"),)
